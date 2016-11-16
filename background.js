1ef96d16f398f89f9553870c8a7ecdfe13b4cc06
Tobias Eilert <tobias.eilert@tvrl.net>
2016-11-16 16:54:05 +0100

    First draft

---
+++ background.js
@@ -0,0 +1,37 @@
+
+var FILTERS = [
+  /ad\d*\./,
+  /^https:\/\/tpc\.googlesyndication\.com/,
+  /doubleclick\.net/,
+  /adition\.com/,
+  /banners\./,
+  /tracking\./,
+  /impression/
+];
+
+var options = {
+  urls: [
+    "http://*/*",
+    "https://*/*"
+  ],
+  types: ["image", "sub_frame"]
+};
+
+function requestCallback(req) {
+  console.log("REQ", req);
+
+  for (var i=0; i != FILTERS.length; ++i) {
+    var f = FILTERS[i];
+
+    if (req.url.match(f)) {
+      console.warn("BLOCKED", req, "as", req.type, "with", f);
+      return { cancel: true };
+    }
+  }
+
+  return null;
+}
+
+chrome.webRequest.onBeforeRequest.addListener(requestCallback, options, ["blocking"]);
+
+console.log("started up...");
